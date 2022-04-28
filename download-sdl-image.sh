414c1a068d33d7f92df01b0274b75f672b8c2eab
Michael Schwarz <michi.schwarz@gmail.com>
2022-04-28 17:59:41 +0200

    Initial checkin of vendored SDL and SDL_image

---
+++ download-sdl-image.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+set -eu
+
+cd "$(dirname "$BASH_SOURCE")"
+
+curl -O 'https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.zip'
---
+++ download-sdl.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+set -eu
+
+cd "$(dirname "$BASH_SOURCE")"
+
+curl -O 'https://libsdl.org/release/SDL2-2.0.16.zip'
